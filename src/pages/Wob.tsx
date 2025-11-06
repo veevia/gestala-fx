dac8fa1d022fc48b789662ce32a7478d1864abb0
Wei <weiwei0108@gmail.com>
2025-11-06 15:03:08 -0600

    config

--- src/pages/Wob.tsx
+++ src/pages/Wob.tsx
@@ -3,6 +3,7 @@ import { useRef } from "react";
 import WobShaderOverlay from "@/components/WobShaderOverlay";
 import { Layout } from "@/components/Layout";
 import { MagicCursor } from "@/components/MagicCursor";
+import { useVideoScrub } from "@/hooks/useVideoScrub";
 import { HeroSection } from "@/components/HeroSection";
 
 const Wob = () => {
